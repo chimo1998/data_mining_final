ab10498294c07426dd1e5aaae40d611507644640
Tsai Wei-Hsuan <whtsai1998@gmail.com>
2020-12-25 10:56:17 +0800

    Implement GBC and data weight

--- dvc_data/models.py
+++ dvc_data/models.py
@@ -1,5 +1,7 @@
 from uci_loader import Loader
 from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+import numpy as np
 
 class Model:
     def __init__(self, encode_y=False):
@@ -8,6 +10,7 @@ class Model:
         self.Xtest = None
         self.Ytest = None
         self.load(encode_y=encode_y)
+        self.train_weight = self.get_sample_weight(self.Ytrain)
         self.model = None
 
     def load(self, encode_y=False):
@@ -18,6 +21,14 @@ class Model:
 
     def predict(self):
         raise NotImplementedError
+
+    def get_sample_weight(self,data):
+        uni, idx = np.unique(data, return_inverse=True)
+        print(uni, idx)
+        w = [1 / (sum(data==u)/len(data)) for u in uni]
+        print(w)
+        return [w[i] for i in idx]
+
         
 class SVM(Model):
     def __init__(self, **kwargs):
@@ -33,3 +44,20 @@ class SVM(Model):
     def predict(self):
         print(self.model)
         return self.model.predict(self.Xtrain)
+
+class GBC(Model):
+    def __init__(self, **kwargs):
+        super().__init__()
+        if len(kwargs) == 0:
+            self.model = GradientBoostingClassifier()
+        else:
+            self.model = GradientBoostingClassifier(**kwargs)
+    
+    def fit(self):
+        self.model.fit(self.Xtrain, self.Ytrain, self.train_weight)
+        print(self.model.score(self.Xtrain, self.Ytrain))
+
+    def predict(self):
+        pred = self.model.predict(self.Xtrain)
+        print(sum(pred == self.Ytrain))
+        return pred
--- dvc_data/test.py
+++ dvc_data/test.py
@@ -1,5 +1,6 @@
-from models import SVM as Model
+from models import GBC as Model
 
-model = Model()
+# GBC
+model = Model(learning_rate=0.03, n_estimators=150, min_samples_split=10, min_samples_leaf=3, init='zero')
 model.fit()
 print(model.predict())
